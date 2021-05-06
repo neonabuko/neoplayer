e0658ae3c88c42959754d844737f145bfd6a7ade
Neo <matheusventureli50@gmail.com>
2021-05-06 00:42:01 -0300

    music player by $Neo

--- theplayer.py
+++ theplayer.py
@@ -1,6 +1,5 @@
 import pygame as pg
 from pygame import mixer, image, display, transform
-import time
 pg.init()
 mixer.init()
 v = 1
@@ -9,6 +8,7 @@ screen = display.set_mode((860, 710), vsync=1)
 holy_img = transform.scale(image.load('megadeth.jpg'), (650, 550))
 teger_img = transform.scale(image.load('tegernako.jpg'), (650, 550))
 back = transform.scale(image.load('land.jpeg'), (850, 700))
+ball = transform.scale(image.load('circle.png'), (25, 25))
 imgs = [holy_img, teger_img]
 pg.font.init()
 helvetica45 = pg.font.SysFont("notomono", 45)
@@ -18,99 +18,109 @@ helvetica25 = pg.font.SysFont("notomono", 25, italic=True)
 helvetica20 = pg.font.SysFont("notomono", 20)
 helvetica16 = pg.font.SysFont("notomono", 16)
 helvetica20_italic = pg.font.SysFont("notomono", 20, italic=True)
-titulo = helvetica45.render('{ NEO PLAYER }', True, (0, 0, 0))
+player = helvetica45.render('{ NEO PLAYER }', True, (0, 0, 0))
+# Tela Inicial:
+display.set_caption('NEO PLAYER')
 opcao1 = helvetica25.render('1 - "Megadeth - Holy Wars"', True, (220, 220, 220))
 opcao2 = helvetica25.render('2 - "Eluveitie - Tegernakô"', True, (220, 220, 220))
 selecione = helvetica16.render('*Selecione a música pelo número', True, (200, 200, 200))
-voltar = helvetica16.render('*Segure ESC para voltar ao menu', True, (200, 200, 200))
-tithol = helvetica35.render('Megadeth - Holy Wars', True, (200, 200, 200))
-titteg = helvetica35.render('Eluveitie - Tegernakô', True, (200, 200, 200))
 author = helvetica20_italic.render('By $Neo', True, (180, 170, 160))
+# Tela da música:
+voltar = helvetica16.render('* ESC = Menu', True, (20, 20, 20))
+tithol = helvetica35.render('Megadeth - Holy Wars', True, (20, 20, 20))
+titteg = helvetica35.render('Eluveitie - Tegernakô', True, (20, 20, 20))
 tits = [tithol, titteg]
-display.set_caption('PLAYER')
+play = pg.image.load('play.png')
+pause = pg.image.load('pause.png')
+play_pause = [play, pause]
+# Músicas:
 som = mixer.Sound('holywars.ogg')
 som2 = mixer.Sound('tegernako.ogg')
 sons = [som, som2]
 som.set_volume(v)
 run = True
-stop = False
 
 
-def rel():
-    s1 = s2 = m1 = m2 = p = 0
-    s = v = 1
-    menos = 0
-    crun = True
-    while crun:
-        while 0 <= s1 <= 9:
-            menos += 1
-            tempo_restante = int(sons[i].get_length()) - menos
-            print(tempo_restante)
-            if tempo_restante < 0:
-                crun = False
-            screen.fill((20, 0, 0))
-            screen.blit(imgs[i], (103, 40))
-            screen.blit(tits[t], (103, 600))
-            screen.blit(voltar, (58, 680))
-            timer = helvetica20.render(f'{m2}{m1}:{s2}{s1}', True, (255, 255, 255))
-            screen.blit(timer, (678, 600))
-            pg.display.update()
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    break
-                key = pg.key.get_pressed()
-                if event.type == pg.KEYDOWN:
-                    if key[pg.K_ESCAPE]:
-                        s = 0
-                        print('Exiting...')
-                        s1 = - 10
-                        break
-                    if p % 2 != 0:
-                        if key[pg.K_SPACE]:
-                            mixer.pause()
-                            print('Pause')
-                    else:
-                        if key[pg.K_SPACE]:
-                            mixer.unpause()
-                            print('Resume')
-                    p += 1
-                    if key[pg.K_KP_PLUS]:
-                        v += 0.1
-                        if v > 1:
-                            v = 1
-                        sons[i].set_volume(v)
-                        print(f'Volume {int(v * 100)}%')
-                    if key[pg.K_KP_MINUS]:
-                        v -= 0.1
-                        if v < 0:
-                            v = 0
-                        sons[i].set_volume(v)
-                        print(f'Volume {int(v * 100)}%')
+def rel(i):
+    v = 1
+    p = pp = 0
+    run = True
+    pr = 120
+    clock = pg.time.Clock()
+    while run:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                run = False
+            key = pg.key.get_pressed()
+            if event.type == pg.KEYDOWN:
+                if key[pg.K_ESCAPE]:
+                    run = False
+                    for i in range(0, len(sons)):
+                        sons[i].stop()
+                    print('Exiting...')
+                if key[pg.K_1]:
+                    i = 0
+                    sons[i].stop()
+                    sons[i].play()
+                    print('Playing "Megadeth - Holy Wars"')
+                if key[pg.K_2]:
+                    som2.stop()
+                    som.stop()
+                    som2.play()
+                    p = 0
+                    print('Playing "Eluveitie - Tegernakô"')
+                if p % 2 == 0:
+                    if key[pg.K_SPACE]:
+                        mixer.pause()
+                        p += 1
+                        pp = 1
+                        print('Pause')
+                else:
+                    if key[pg.K_SPACE]:
+                        mixer.unpause()
+                        p += 1
+                        pp = 0
+                        print('Resume')
+                if key[pg.K_KP_PLUS]:
+                    v += 0.1
                     if v > 1:
                         v = 1
-                        print('Volume Max')
+                    som.set_volume(v)
+                    som2.set_volume(v)
+                    print(f'Volume {int(v * 100)}%')
+                if key[pg.K_KP_MINUS]:
+                    v -= 0.1
                     if v < 0:
                         v = 0
-                        print('Volume Min')
-            s1 += 1
-            if 0 <= s1 <= 9:
-                print(f'{m2}{m1}:{s2}{s1}')
-                time.sleep(s)
-        if s1 > - 8:
-            s1 = 0
-            s2 += 1
-            if s2 == 6:
-                s2 = 0
-                m1 += 1
-            if m1 == 10:
-                m1 = 0
-                m2 += 1
-            print(f'{m2}{m1}:{s2}{s1}')
-            time.sleep(s)
-        else:
-            crun = False
-            sons[i].stop()
-            p = 0
+                    som.set_volume(v)
+                    som2.set_volume(v)
+                    print(f'Volume {int(v * 100)}%')
+                if v >= 1:
+                    v = 1
+                    print('Volume Max')
+                if v <= 0:
+                    v = 0
+                    print('Volume Min')
+        clock.tick(100)
+        s = sons[i].get_length()
+        d = 6 / s
+        pr += d
+        if p % 2 != 0:
+            pr -= d
+        if pr > 690:
+            pr = 690
+        volume = helvetica20.render(f'Volume {int(v * 100)}%', True, (20, 20, 20))
+        preto = (0, 0, 0)
+        sombra = (60, 60, 60)
+        screen.fill((140, 140, 140))
+        screen.blit(imgs[i], (103, 40))
+        screen.blit(tits[t], (103, 600))
+        screen.blit(voltar, (600, 690))
+        pg.draw.rect(screen, preto, (120, 661, 600, 3))
+        screen.blit(ball, (pr, 650))
+        screen.blit(volume, (620, 610))
+        screen.blit(play_pause[pp], (94, 650))
+        pg.display.update()
 
 
 while run:
@@ -125,69 +135,20 @@ while run:
                 print('Exiting...')
             if busy == 0:
                 if key[pg.K_1] or key[pg.K_KP_1]:
-                    som.play()
                     p = i = t = 0
+                    sons[i].play()
                     print('Playing "Megadeth - Holy Wars"')
-                    rel()
+                    rel(i)
                 if key[pg.K_2] or key[pg.K_KP_2]:
-                    som2.play()
                     p = 0
                     t = i = 1
-                    print('Playing "Eluveitie - Tegernakô"')
-                    rel()
-            else:
-                if key[pg.K_1]:
-                    som.stop()
-                    som2.stop()
-                    som.play()
-                    p = 0
-                    print('Playing "Megadeth - Holy Wars"')
-                if key[pg.K_2]:
-                    som2.stop()
-                    som.stop()
                     som2.play()
-                    p = 0
                     print('Playing "Eluveitie - Tegernakô"')
-                if p % 2 != 0:
-                    if key[pg.K_SPACE]:
-                        mixer.pause()
-                        print('Pause')
-                else:
-                    if key[pg.K_SPACE]:
-                        mixer.unpause()
-                        print('Resume')
-            if key[pg.K_RETURN]:
-                som.stop()
-                som2.stop()
-                stop = True
-                print('Player stopped')
-            if key[pg.K_KP_PLUS]:
-                v += 0.1
-                if v > 1:
-                    v = 1
-                som.set_volume(v)
-                som2.set_volume(v)
-                print(f'Volume {int(v * 100)}%')
-            if key[pg.K_KP_MINUS]:
-                v -= 0.1
-                if v < 0:
-                    v = 0
-                som.set_volume(v)
-                som2.set_volume(v)
-                print(f'Volume {int(v * 100)}%')
-            p += 1
-            if v > 1:
-                v = 1
-                print('Volume Max')
-            if v < 0:
-                v = 0
-                print('Volume Min')
-            if stop:
-                p = 0
-                stop = False
+                    rel(i)
+
     screen.fill((0, 10, 25))
     screen.blit(back, (5, 5))
-    screen.blit(titulo, (250, 80))
+    screen.blit(player, (250, 80))
     screen.blit(opcao1, (100, 450))
     screen.blit(opcao2, (100, 500))
     screen.blit(selecione, (50, 680))
